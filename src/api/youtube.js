6dbbf70ca29e57b7d8e69dacc3b46ef8e13c6037
NightHunter311 <obliviongod16@gmail.com>
2020-10-01 23:22:51 +0530

    NEVER put up api keys publically

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
+import KEY from '../data.js'
 
-const KEY = "AIzaSyAqXbjM4LlNVqfRdY-i9QpyDdsxN6TvZ74";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
